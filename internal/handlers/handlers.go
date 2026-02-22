1d40e2cb78e18ef70c1e771b1bb026fea2a2362d
Larry <ivangelarry@gmail.com>
2026-02-21 19:58:19 -0600

    Implement append-only write

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"fmt"
+	"os"
+	"path"
 
 	"github.com/ivange94/junkdb/pkg/router"
 )
@@ -12,8 +14,22 @@ func Write(ctx *router.Ctx) error {
 	if err != nil {
 		return fmt.Errorf("error reading request body: %w", err)
 	}
-	fmt.Printf("Writing %s,%s\n", key, string(bodyBytes))
-	return nil
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("error reading users home directory: %w", err)
+	}
+	dataFilePath := path.Join(home, ".junkdb", "data")
+	file, err := os.OpenFile(dataFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // TODO: handle concurrent access
+	if err != nil {
+		return fmt.Errorf("error reading data file path: %w", err)
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	entry := fmt.Sprintf("%s,%s\n", key, string(bodyBytes))
+	_, err = file.WriteString(entry)
+	return err
 }
 
 func Read(ctx *router.Ctx) error {
